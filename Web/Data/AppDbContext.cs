d45eab3580b8d1a6f77834636295c986494ab571
Ивета Иванова <ivi251311@gmail.com>
2025-01-22 10:46:55 +0200

    Първа промяна

---
+++ Web/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web.Models;
+namespace Web.Data   
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Genre>().HasData(
+                new Genre { Id = 1, Name = "Fiction" },
+                new Genre { Id = 2, Name = "Non-Fiction" },
+                new Genre { Id = 3, Name = "Horror" },
+                new Genre { Id = 4, Name = "Crime" },
+                new Genre { Id = 5, Name = "Fantasy" }
+                );
+        }
+    }
+
+}
---
+++ Web/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace Web.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int Year { get; set; }
+
+
+        public int GenreId { get; set; }
+
+        public Genre Genre { get; set; }
+    }
+}
